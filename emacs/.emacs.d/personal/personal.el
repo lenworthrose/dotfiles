070c600f0d54c0591af3e67fa0492ac8f0bcb4c0
Lenworth Rose <lenworthrose@gmail.com>
2016-03-16 15:47:42 -0300

    Add emacs and lein dotfiles

---
+++ emacs/.emacs.d/personal/personal.el
@@ -0,0 +1,101 @@
+;;; personal.el -- Josh's EMACS config
+;;
+;;; Commentary:
+;;
+;; My config for Emacs, using the prelude settings pack.
+;;
+;;; License:
+;;
+;;The MIT License (MIT)
+;;
+;;Copyright (c) 2013 Josh Comer
+;;
+;;Permission is hereby granted, free of charge, to any person obtaining a copy of
+;;this software and associated documentation files (the "Software"), to deal in
+;;the Software without restriction, including without limitation the rights to
+;;use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+;;the Software, and to permit persons to whom the Software is furnished to do so,
+;;subject to the following conditions:
+;;
+;;The above copyright notice and this permission notice shall be included in all
+;;copies or substantial portions of the Software.
+;;
+;;THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+;;FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+;;COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+;;IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;;CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;;
+;;; Code:
+
+(prelude-require-packages '(highlight-symbol
+                            paredit
+                            clj-refactor
+                            key-chord
+                            avy
+                            rainbow-delimiters
+                            restclient))
+
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+(eval-after-load 'clojure-mode
+  '(progn
+     (add-hook 'clojure-mode-hook
+               (lambda ()
+                 (highlight-symbol-mode 1)
+                 (smartparens-mode -1)
+                 (paredit-mode 1)
+                 (clj-refactor-mode 1)
+                 (rainbow-delimiters-mode 1)
+                 (cljr-add-keybindings-with-prefix "C-c C-g")) t)))
+
+(eval-after-load 'cider
+  '(progn
+     (add-hook 'cider-repl-mode-hook #'paredit-mode)
+     (add-hook 'cider-mode-hook #'eldoc-mode)
+     (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)))
+
+(global-set-key (kbd "C-c C-s") #'cider-format-buffer)
+(setq cider-font-lock-dynamically '(macro core function var))
+(setq cider-overlays-use-font-lock t)
+
+(setq avy-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n))
+
+(key-chord-define-global "xx" 'smex)
+
+;; Try just a visible bell
+;;(setq visible-bell 1)
+;; If that drives you crazy turn it all off
+(setq ring-bell-function 'ignore)
+
+;; Make tabs 4 spaces
+(setq tab-width 4)
+
+;; Set whitespace line length
+(setq whitespace-line-column 120)
+
+;;Turn off scrollbars
+(scroll-bar-mode -1)
+
+;;Make paredit look cool
+(eval-after-load "paredit"
+  '(diminish 'paredit-mode " π"))
+
+;;Turn off guru mode
+;;(setq guru-warn-only t)
+(setq prelude-guru nil)
+
+(disable-theme 'zenburn)
+
+(setq highlight-symbol-mode 0)
+;;(setq solarized-use-more-italic t)
+
+(load-theme 'sanityinc-tomorrow-night t)
+
+(global-linum-mode t)
+(setq cljr-inject-dependencies-at-jack-in nil)
+
+(provide 'personal)
+
+;;; personal.el ends here
---
+++ lein/.lein/profiles.clj
@@ -0,0 +1,23 @@
+{:user {:plugins [[lein-kibit "0.1.2"]
+                  [lein-ancient "0.6.7"]
+                  [lein-environ "1.0.2"]
+                  [lein-vanity "0.2.0"]
+                  [codox "0.9.0"]
+                  [lein-exec "0.3.5"]
+                  [quickie "0.4.1"]
+                  [test2junit "1.1.2"]]
+        :aliases {"ccheck" ^{:doc "Clean and check the source"}
+                  ["do" ["clean"] ["check"]]
+                  "cinstall" ^{:doc "Clean and install locally"}
+                  ["do" ["clean"] ["install"]]
+                  "start" ^{:doc "Trampoline run"}
+                  ["trampoline" "run"]
+                  "cstart" ^{:doc "Clean and start"}
+                  ["do" ["clean"] ["start"]]
+                  "ttest" ^{:doc "Run tests in test profile"}
+                  ["with-profile" "test" "test"]
+                  "sancient" ^{:doc "Check all deps including snapshots"}
+                  ["ancient" ":allow-all"]}}
+ :repl {:plugins [#_[refactor-nrepl "2.0.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
