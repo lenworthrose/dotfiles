420e59667a3fd8cc7aa2510c4486d667dd213840
Lenworth Rose <lenworthrose@gmail.com>
2016-10-28 13:41:37 -0300

    Update emacs personal.el

--- emacs/.emacs.d/personal/personal.el
+++ emacs/.emacs.d/personal/personal.el
@@ -29,15 +29,16 @@
 ;;
 ;;; Code:
 
-(prelude-require-packages '(highlight-symbol
+(prelude-require-packages '(color-theme-sanityinc-tomorrow
                             paredit
                             clj-refactor
                             key-chord
                             avy
                             rainbow-delimiters
-                            restclient))
+                            aggressive-indent))
 
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("CHANGELOG\\.md\\'" . gfm-mode))
 
 (eval-after-load 'clojure-mode
   '(progn
@@ -48,17 +49,19 @@
                  (paredit-mode 1)
                  (clj-refactor-mode 1)
                  (rainbow-delimiters-mode 1)
-                 (cljr-add-keybindings-with-prefix "C-c C-g")) t)))
+                 (aggressive-indent-mode 1)
+                 (yas-minor-mode 1)
+                 (eldoc-mode +1)
+                 (cljr-add-keybindings-with-prefix "C-c C-l")) t)))
 
 (eval-after-load 'cider
   '(progn
      (add-hook 'cider-repl-mode-hook #'paredit-mode)
-     (add-hook 'cider-mode-hook #'eldoc-mode)
-     (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)))
-
-(global-set-key (kbd "C-c C-s") #'cider-format-buffer)
-(setq cider-font-lock-dynamically '(macro core function var))
-(setq cider-overlays-use-font-lock t)
+     (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+     (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
+     (setq cider-font-lock-dynamically '(macro core function var))
+     (setq cider-overlays-use-font-lock t)
+     (global-set-key (kbd "C-c C-s") #'cider-format-buffer)))
 
 (setq avy-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n))
 
@@ -88,18 +91,53 @@
 
 (disable-theme 'zenburn)
 
-(setq highlight-symbol-mode 0)
-;;(setq solarized-use-more-italic t)
+(setq solarized-use-more-italic t)
 
 (load-theme 'sanityinc-tomorrow-night t)
 
-(global-linum-mode t)
+;;(set-default-font "Pragmata Pro Mono")
+
+;;(custom-set-faces
+;; '(default ((t (:height 150 :family "Pragmata Pro Mono")))))
+
+;;(setq cider-inject-dependencies-at-jack-in nil)
+
+;;(aggressive-indent-global-mode 1)
+
 ;;(setq cljr-inject-dependencies-at-jack-in nil)
+
+(global-linum-mode t)
 
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "<insert>"))
 (global-set-key (kbd "C-<insert>") 'overwrite-mode)
 
+(setq cider-font-lock-dynamically '(macro core function var))
+(setq cider-overlays-use-font-lock t)
+
 (provide 'personal)
 
 ;;; personal.el ends here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
