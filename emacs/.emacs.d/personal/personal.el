59f121ffb19813ff053b4d6df9a2ac17c8b9a623
Lenworth Rose <lenworthrose@gmail.com>
2016-11-01 20:15:38 -0300

    Add company to emacs personal.el

--- emacs/.emacs.d/personal/personal.el
+++ emacs/.emacs.d/personal/personal.el
@@ -29,7 +29,8 @@
 ;;
 ;;; Code:
 
-(prelude-require-packages '(color-theme-sanityinc-tomorrow
+(prelude-require-packages '(company
+                            color-theme-sanityinc-tomorrow
                             paredit
                             clj-refactor
                             key-chord
@@ -60,6 +61,8 @@
      (add-hook 'cider-repl-mode-hook #'paredit-mode)
      (add-hook 'cider-repl-mode-hook #'eldoc-mode)
      (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
+     (add-hook 'cider-repl-mode-hook #'company-mode)
+     (add-hook 'cider-mode-hook #'company-mode)
      (setq cider-font-lock-dynamically '(macro core function var))
      (setq cider-overlays-use-font-lock t)
      (global-set-key (kbd "C-c C-s") #'cider-format-buffer)))
